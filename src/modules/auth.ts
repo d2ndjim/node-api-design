1cc263df4a6edee07e880f6243cd16a1a3bcdec7
Lekan <jimoridwanlekan@gmail.com>
2023-05-04 18:46:55 +0100

    Validate bearer token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -13,4 +13,25 @@ export const protect = (req, res, next) => {
     res.send("Not authorized");
     return;
   }
+
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    console.log("here");
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    console.log(payload);
+    next();
+    return;
+  } catch (e) {
+    console.error(e);
+    res.status(401);
+    res.send("Not authorized");
+    return;
+  }
 }
