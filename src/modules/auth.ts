026f0ba7dfd27dbba6b759f178fb7c99ee1d57c7
Lekan <jimoridwanlekan@gmail.com>
2023-05-04 18:57:34 +0100

    Compare and hash passwords

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,13 @@
 import jwt from 'jsonwebtoken';
+import * as bcrypt from "bcrypt";
+
+export const comparePasswords = (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const hashPassword = (password) => {
+  return bcrypt.hash(password, 5);
+};
 
 export const createJWT = (user) => {
   const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET);
