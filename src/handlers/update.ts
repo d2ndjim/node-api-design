a62a48f7109c09d7e712f255a65551e6c733e0c9
Lekan <jimoridwanlekan@gmail.com>
2023-05-11 16:43:17 +0100

    Add update get handlers

---
+++ src/handlers/update.ts
@@ -0,0 +1,25 @@
+import prisma from "../db";
+
+export const getOneUpdate = async (req, res) => {
+  const update = await prisma.update.findUnique({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json({data: update});
+};
+
+export const getUpdates = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      updates: true,
+    },
+  });
+  const updates = products.reduce((acc, product) => {
+    return [...acc, ...product.updates];
+  }, []);
+  res.json({data: updates});
+}
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body } from "express-validator";
 import { handleInputErrors } from './modules/middleware';
 import { createProduct, deleteProduct, getOneProduct, getProducts, updateProduct } from './handlers/product';
+// import { getUpdates } from './handlers/update';
 
 const router = Router();
 
@@ -15,25 +16,19 @@ router.delete("/product/:id", deleteProduct);
  * Update
  */
 
-router.get("/update", (req, res) => {});
-
+// router.get("/update", getUpdates);
 router.get("/update/:id", (req, res) => {});
-
 router.post("/update", 
   [body("title").exists().isString(), 
     body("body").exists().isString(), 
   ], handleInputErrors, (req, res) => {
-  
 });
-
 router.put("/update/:id", [body("title").optional(), 
     body("body").optional(), 
     body('status').isIn(["IN_PROGRESS", "LIVE", "DEPRECATED"]), 
     body("version").optional(),
-  ], handleInputErrors, (req, res) => {
-  
+  ], handleInputErrors, (req, res) => { 
 });
-
 router.delete("/update/:id", (req, res) => {});
 
 /**
