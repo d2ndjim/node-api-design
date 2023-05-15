d37d66a873189cbc4534c6be2e0ce38b18bd6e4f
Lekan <jimoridwanlekan@gmail.com>
2023-05-15 18:50:21 +0100

    Test create new user route with supertest

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,5 +1,11 @@
+import * as user from "../user";
+
 describe("user handler", () => {
-  it("should do a thing", async () => {
-    expect("something").toBe("something");
+  it("should create a new user", async () => {
+    const req = {body: {username: "test", password: "test"}};
+    const res = {json({token}) {
+      expect(token).toBeTruthy();
+    }};
+    await user.createNewUser(req, res);
   });
 });
